53ef378c31b87c856910fde3d525b09b047a8f50
tam <tourist.tam@gmail.com>
2013-03-14 18:29:10 +0000

    added repository method to retrieve audit with list of used forms

--- Entity/Repository/AuditRepository.php
+++ Entity/Repository/AuditRepository.php
@@ -13,4 +13,45 @@ class AuditRepository extends EntityRepository
         $qb->setMaxResults( $limit );
         return $qb->getQuery()->execute();
     }
+
+    public function getAuditGroupByForms()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select( 'a' )->from( 'CiscoSystemsAuditBundle:Audit', 'a' );
+        $entities = $qb->getQuery()->getResult();
+
+        $forms = array();
+        $result = array();
+
+        foreach ( $entities as $entity )
+        {
+            $id = $entity->getAuditReference()->getId();
+            if( !array_key_exists( $id, $forms ) )
+            {
+                $forms[$id] = array( $entity->getAuditForm() );
+            }
+            else
+            {
+                if( !in_array( $entity->getAuditForm(), $forms ))
+                {
+                    array_push($forms[$id], $entity->getAuditForm());
+                }
+            }
+        }
+
+        foreach ( $entities as $entity )
+        {
+            $row = array(
+                'id' => $entity->getId(),
+                'auditReference' => $entity->getAuditReference(),
+                'auditForm' => $entity->getAuditForm(),
+                'auditingUser' => $entity->getAuditingUser(),
+                'flag' => $entity->getFlag(),
+                'totalScore' => $entity->getTotalScore(),
+                'usedforms' => $forms[$entity->getAuditReference()->getId()]
+            );
+            $result[] = $row;
+        }
+        return $result;
+    }
 }
