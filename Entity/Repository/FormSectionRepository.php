ede04cc247f08e802aca19926d9d6a6c58c38465
tam <tourist.tam@gmail.com>
2013-09-19 12:10:49 +0100

    refactor existing repo method on FormSection

--- Entity/Repository/FormSectionRepository.php
+++ Entity/Repository/FormSectionRepository.php
@@ -8,22 +8,20 @@ use CiscoSystems\AuditBundle\Entity\Section;
 
 class FormSectionRepository extends SortableRepository
 {
-    public function qbSectionRelation( $form, $section )
+    public function qbRelation( Form $form, Section $section )
     {
         return $this->createQueryBuilder( 'r' )
-                    ->join( 'CiscoSystemsAuditBundle:Form', 'f', 'WITH', 'f.formSection = r' )
-                    ->join( 'CiscoSystemsAuditBundle:Section', 's', 'WITH', 's.formSection = r' )
-                    ->where( 's = :section' )
-                    ->andWhere( 'f = :form' )
+                    ->where( 'r.section = :section' )
+                    ->andWhere( 'r.form = :form' )
                     ->addParameters( array(
                         'section'   => $section,
                         'form'      => $form,
                     ));
     }
 
-    public function getSectionRelation( $form, $section )
+    public function getRelation( Form $form, Section $section )
     {
-        return $this->qbSectionRelation( $form, $section )
+        return $this->qbRelation( $form, $section )
                     ->getQuery()
                     ->getResult();
     }
